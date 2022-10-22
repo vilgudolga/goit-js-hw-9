7bddeb71ad4ba278a2fecc905d5a71339dbe05ad
vilgudolga <95307871+vilgudolga@users.noreply.github.com>
2022-10-22 15:59:52 +0200

    HW-08

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
-
+import { throttle } from 'lodash';
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+player.on('timeupdate',throttle(({ seconds }) => {
+    localStorage.setItem('videoplayer-current-time', `${seconds}`);
+  }, 1000)
+);
+const savedTime = localStorage.getItem('videoplayer-current-time');
+player
+  .setCurrentTime(savedTime)
+  .then(function (seconds) {
+})
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError': break;
+      default: break;
+}});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,24 @@
-
+import { throttle } from "lodash";
+const form = document.querySelector('.feedback-form');
+const email = document.querySelector('input[name="email"]');
+const message = document.querySelector('textarea[name="message"]');
+let lastSession = {};
+form.addEventListener('input', throttle(
+    () => {
+        localStorage.setItem("feedback-form-state", JSON.stringify({ email: email.value, message: message.value }))
+    }, 500));
+try {
+    lastSession = JSON.parse(localStorage.getItem('feedback-form-state'));
+    if(lastSession!==null){
+        email.value=lastSession.email;
+        message.value=lastSession.message;
+     }
+    } catch (err) {
+    console.log('Parse error:' + err.message);
+}
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    console.log({ email: email.value, message: message.value });
+    form.reset();
+    localStorage.removeItem('feedback-form-state');
+})
