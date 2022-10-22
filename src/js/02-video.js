1595f22a791856f45d9627300ec191aa0809fa9c
vilgudolga <95307871+vilgudolga@users.noreply.github.com>
2022-10-22 17:12:47 +0200

    hw-8

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,17 +1,28 @@
 import { throttle } from 'lodash';
+
 const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
+
 player.on('timeupdate',throttle(({ seconds }) => {
     localStorage.setItem('videoplayer-current-time', `${seconds}`);
   }, 1000)
 );
+
 const savedTime = localStorage.getItem('videoplayer-current-time');
+
 player
   .setCurrentTime(savedTime)
   .then(function (seconds) {
-})
+    // seconds = the actual time that the player seeked to
+  })
   .catch(function (error) {
     switch (error.name) {
-      case 'RangeError': break;
-      default: break;
-}});
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,24 +1,35 @@
-import { throttle } from "lodash";
-const form = document.querySelector('.feedback-form');
-const email = document.querySelector('input[name="email"]');
-const message = document.querySelector('textarea[name="message"]');
-let lastSession = {};
-form.addEventListener('input', throttle(
-    () => {
-        localStorage.setItem("feedback-form-state", JSON.stringify({ email: email.value, message: message.value }))
+import { throttle } from 'lodash';
+const form = document.querySelector('form');
+const email = form.querySelector('input');
+const text = form.querySelector('textarea');
+
+form.addEventListener(
+    'input',
+        throttle((event) => {
+            const input = { email: form.elements.email.value, message: form.elements.message.value };
+            localStorage.setItem('feedback-form-state', JSON.stringify(input));
     }, 500));
-try {
-    lastSession = JSON.parse(localStorage.getItem('feedback-form-state'));
-    if(lastSession!==null){
-        email.value=lastSession.email;
-        message.value=lastSession.message;
-     }
-    } catch (err) {
-    console.log('Parse error:' + err.message);
-}
-form.addEventListener('submit', e => {
-    e.preventDefault();
-    console.log({ email: email.value, message: message.value });
-    form.reset();
-    localStorage.removeItem('feedback-form-state');
-})
+    
+    form.removeEventListener('blur',()=> {
+        document.style.background = pink;
+    });
+    
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const { elements: { email, message } } = e.currentTarget;
+        console.log({ email: email.value, message: message.value });
+    
+        e.currentTarget.reset();
+        localStorage.clear();
+    });
+    
+    const storage = localStorage.getItem('feedback-form-state');
+    const parseStorageData = JSON.parse(storage);
+    const tryFoo = () => {
+        if (parseStorageData !== null) {
+            email.value = parseStorageData.email;
+            text.value = parseStorageData.message;
+        };
+    };
+    
+    tryFoo();
