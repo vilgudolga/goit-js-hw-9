d08c74a410d8ab5d6274bf33e318066722c5243a
vilgudolga <95307871+vilgudolga@users.noreply.github.com>
2022-10-23 17:21:30 +0200

    HW-08

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,7 +4,9 @@ import Player from '@vimeo/player';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-player.on('timeupdate',throttle(({ seconds }) => {
+player.on(
+  'timeupdate',
+  throttle(({ seconds }) => {
     localStorage.setItem('videoplayer-current-time', `${seconds}`);
   }, 1000)
 );
@@ -13,17 +15,13 @@ const savedTime = localStorage.getItem('videoplayer-current-time');
 
 player
   .setCurrentTime(savedTime)
-  .then(function (seconds) {
-    // seconds = the actual time that the player seeked to
-  })
+  .then(function (seconds) {})
   .catch(function (error) {
     switch (error.name) {
       case 'RangeError':
-        // the time was less than 0 or greater than the video’s duration
         break;
 
       default:
-        // some other error occurred
         break;
     }
   });
