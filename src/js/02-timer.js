f815e11a9aa31ed3f935abb00cfd2e9ed3e57ed7
vilgudolga <95307871+vilgudolga@users.noreply.github.com>
2022-11-09 18:57:21 +0100

    HW-9

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,25 +1,11 @@
+import Notiflix from 'notiflix';
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 
-const options = {
-  enableTime: true,
-  time_24hr: true,
-  defaultDate: new Date(),
-  minuteIncrement: 1,
-  onClose(selectedDates) {
-    const selected = selectedDates[0].getTime();
-    if (selected < options.defaultDate.getTime()) {
-      alert('Please choose a date in the future');
-    } else {
-      startBtn.disabled = false;
-      return (selectedDates = selectedDates[0]);
-    }
-  },
-};
-const picker = document.querySelector('#datetime-picker');
 const startBtn = document.querySelector('[data-start]');
-startBtn.disabled = false;
-function convertMs(ms) {
+startBtn.disabled = true;
+
+const convertMs = ms => {
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
@@ -30,40 +16,57 @@ function convertMs(ms) {
   const minutes = Math.floor(((ms % day) % hour) / minute);
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
   return { days, hours, minutes, seconds };
-}
+};
+let selectedDate = null;
 
-flatpickr(picker, options);
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    const selected = selectedDates[0].getTime();
+    if (selected < options.defaultDate.getTime()) {
+     Notiflix.Notify.failure('Please choose a date in the future');
+    } else {
+      startBtn.disabled = false;
+      //console.log(selectedDates[0]);//
+      return selectedDate = selectedDates[0];
+    }
+  },
+};
 
+flatpickr('#datetime-picker', options);
 function addLeadingZero(value) {
   if (value < 10) {
-    return (value = '0' + value);
+    return value.toString().padStart(2, '0');
   } else {
     return value;
   }
 }
 
-let days = document.querySelector('[data-days]');
-let hours = document.querySelector('[data-hours]');
-let minutes = document.querySelector('[data-minutes]');
-let seconds = document.querySelector('[data-seconds]');
+let daysHtml = document.querySelector('[data-days]');
+let hoursHtml = document.querySelector('[data-hours]');
+let minutesHtml = document.querySelector('[data-minutes]');
+let secondsHtml = document.querySelector('[data-seconds]');
 
 const getDifference = () => {
   const todayTime = new Date().getTime();
-  const difference = selectedDates.getTime() - todayTime;
+  const difference = selectedDate.getTime() - todayTime;
   if (difference < 1000) {
     clearInterval(timerId);
   }
   const resultDays = convertMs(difference).days;
-  days.textContent = addLeadingZero(resultDays);
+  daysHtml.textContent = addLeadingZero(resultDays);
 
   const resultHours = convertMs(difference).hours;
-  hours.textContent = addLeadingZero(resultHours);
+  hoursHtml.textContent = addLeadingZero(resultHours);
 
   const resultMinutes = convertMs(difference).minutes;
-  minutes.textContent = addLeadingZero(resultMinutes);
+  minutesHtml.textContent = addLeadingZero(resultMinutes);
 
   const resultSeconds = convertMs(difference).seconds;
-  seconds.textContent = addLeadingZero(resultSeconds);
+  secondsHtml.textContent = addLeadingZero(resultSeconds);
 };
 
 let timerId = null;
